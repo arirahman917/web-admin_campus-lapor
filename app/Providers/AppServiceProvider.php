cadeef6142179a72dfcb422050a1fb43726a5fdf
Ari Rahman <arryrahmand5@gmail.com>
2026-06-03 00:19:58 +0700

    fix: force https scheme for production assets

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -5,6 +5,7 @@ namespace App\Providers;
 use App\Services\ChatStore;
 use Illuminate\Support\ServiceProvider;
 use Illuminate\Support\Facades\View;
+use Illuminate\Support\Facades\URL;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -21,6 +22,10 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
+        if (config('app.env') === 'production') {
+            URL::forceScheme('https');
+        }
+
         View::composer('layouts.app', function ($view) {
             $adminUsername = session('auth_username', 'admin1');
             $campusKey = session('auth_kode_kampus')
